93bdb69d8c5b71c97c6c22c31df7dfd2d623e14b
Juan Garcia <juan.off.182@gmail.com>
2021-11-11 16:48:43 -0500

    Finished all the common pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
-  nombre: string = 'juan CaRlos';
-  valor: number = 1000;
-  obj = {
-    nombre: 'Juan'
-  }
+  constructor(private primengConfig: PrimeNGConfig) {}
 
-  mostrarNombre() {
-    console.log(this.nombre);
-    console.log(this.valor);
-    console.log(this.obj);
-    
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 }
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +7,67 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  // i18nSelect
+  nombre: string = 'Andrea';
+  genero: string = 'femenino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino' : 'invitarlo',
+    'femenino'  : 'invitarla'
   }
+
+  // i18nPlural
+  clientes: string[] = ['Maria','Juan','Ivan','Carlos','Lina'];
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+
+  }
+
+  cambiarCliente() {
+    this.nombre = 'Juca';
+    this.genero = 'masculino';
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
+  }
+
+  // KeyValue Pipe
+
+  persona = {
+    nombre: 'Juan',
+    edad: 28,
+    direccion: 'Tulua, Colombia'
+  }
+
+  // Json Pipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    }
+  ]
+
+  // Async Pipe
+  miObservable = interval(2000);
+
+  valorPromesa = new Promise( (resolve, reject) => { 
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 
 }
